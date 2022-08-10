aa7a411c82e80e787f2e70d1d20d41553458ee87
Di <dimas2343@mail.ru>
2022-08-10 15:52:43 +0300

    sqr number

---
+++ 002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого.
+
+Console.WriteLine("Введеите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введеите первое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num2 / num1 = num1)
+{
+    
+}
