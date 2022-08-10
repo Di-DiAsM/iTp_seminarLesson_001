7e4d9ca631778b6edfbc4314f13efc5c95dd6e8e
Di <dimas2343@mail.ru>
2022-08-10 16:08:04 +0300

    n2 = n1*n1

---
+++ 002_n2=n х n/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первого.
+
+Console.WriteLine("Введеите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введеите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n2 == n1 * n1)
+{
+    Console.WriteLine(n2 + " является квадратом " + n1);
+}
+else
+{
+    Console.WriteLine(n2 + " не является квадратом " + n1);
+}
