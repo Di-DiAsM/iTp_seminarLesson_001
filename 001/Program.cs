eefcaa5ec4f57e9486a10fd0bcb12d31e174e55e
Di <dimas2343@mail.ru>
2022-08-10 15:50:42 +0300

    sqr number

---
+++ 001/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.Write("Введеите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sqr = num * num;
+Console.WriteLine(sqr);
