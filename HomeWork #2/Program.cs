45b0d336e7a636fffcb7cf3f7597c14806ae9464
Di <dimas2343@mail.ru>
2022-08-11 03:35:47 +0300

    HomeWork

---
+++ HomeWork #2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{
+    Console.WriteLine("Число " + n1 + " больше числа " + n2);
+}
+else if(n1 == n2)
+{
+    Console.WriteLine("Число " + n1 + " равно числу " + n2);
+}
+else
+{
+    Console.WriteLine("Число " + n2 + " больше числа " + n1);
+}
---
+++ HomeWork #4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+
+if(n2 > max)
+{
+    max = n2;
+}
+if(n3 > max)
+{
+    max = n3;
+}
+Console.WriteLine("Максимальное число: " + max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
