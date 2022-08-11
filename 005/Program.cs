3e31ca6a79122e55ed1318d15b50df1211e5e92a
Di <dimas2343@mail.ru>
2022-08-11 03:17:41 +0300

    456 -> 6

---
+++ 005/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+//	456 -> 6
+//	782 -> 2
+//	918 -> 8
+
+Console.WriteLine("Введите трехзначное числоЖ ");
+int num = Convert.ToInt32(Console.ReadLine());
+int lastDigit = num % 10;
+
+if (num < 99)
+{
+    Console.WriteLine("неправильный ввод");
+}
+else if (num > 999)
+{
+    Console.WriteLine("неправильный ввод");
+}
+else
+{
+    Console.WriteLine(lastDigit);
+}
